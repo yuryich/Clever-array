841789ade5294ef69ef8132540110c8be63945b5
Neto test <yury120405@gmail.com>
2024-09-11 23:41:31 +0400

    Initial commit

---
+++ Умный массив/Умный массив.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <stdexcept> // для обработки исключений
+
+using namespace std;
+
+class Clever_array
+{
+public:
+	Clever_array(int p_size) : size(p_size), current_size(0) {
+		if (size <= 0) {
+			throw std::invalid_argument("Размер массива должен быть больше нуля.");
+		}
+		array = new int[size]; 
+	}
+
+	void append(int value) {
+		if (current_size >= size) {
+			throw out_of_range("Превышен размер массива.");
+		}
+		array[current_size++] = value;
+	}
+
+	int get_element(int ind) {
+		if (ind < 0 || ind >= current_size) {
+			throw out_of_range("Некорректный индекс.");
+		}
+		return array[ind];
+	}
+
+	~Clever_array() {
+		delete[] array; 
+	}
+
+private:
+	int* array;         
+	int size;           
+	int current_size;   
+};
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	try {
+		Clever_array arr(5);
+		arr.append(1);
+		arr.append(4);
+		arr.append(155);
+		arr.append(14);
+		arr.append(15);
+
+		cout << arr.get_element(1) << endl;
+		cout << arr.get_element(10) << endl;
+	}
+	catch (const exception& ex) {
+		cout << ex.what() << endl;
+	}
+}
